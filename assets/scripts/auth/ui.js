0904cfed9d4f3a117d5c549dd0da042fc9a02ff8
demonte <>
2021-04-14 09:03:15 -0400

    added my auth actions and a nav bar

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,77 @@
+const store = require('./../store')
+const events = require('./events')
+const api = require('./api')
+
+
+const signUpSuccess = function(response) {
+  $('#message').text('Thank you for signing up!')
+  $('#sign-up').trigger('reset')
+}
+
+const signUpFailure = function(response) {
+  $('#error-message').text('Sign up failed, try again')
+  $('#error-message').trigger('reset')
+}
+
+const signInSuccess = function(response) {
+  store.user = response.user
+$('#message').text('Welcome to PAWSOMEDogs!')
+  $('#sign-in').trigger('reset')
+  $('#change-password').show()
+  $('#nav-bar').show()
+  $('#sign-out').show()
+  $('#sign-up').hide()
+  $('#sign-in').hide()
+
+  $('#dogs-index').show()
+  $('#dogs-create').show()
+  $('#dogs-update').show()
+  $('#dogs-destroy').show()
+}
+
+const signInFailure = function(response) {
+  $('#error-message').text('Sign In failed, try again')
+  $('form').trigger('reset')
+}
+
+const changePasswordSuccess = function(response) {
+  $('#message').text('Your password has been successfully changed!')
+  $('#change-password').trigger('reset')
+}
+
+const changePasswordFailure = function(response) {
+  $('#error-message').text('Change Password failed, try again')
+  $('form').trigger('reset')
+}
+
+const signOutSuccess = function(response) {
+
+  $('#message').text('Thank you for your input on our PawFriends!')
+    $('#sign-out').trigger('reset')
+    $('#sign-in').show()
+    $('#sign-up').show()
+    $('#change-password').hide()
+    $('#sign-out').hide()
+
+    $('#dogs-index').hide()
+    $('#dogs-create').hide()
+    $('#dogs-update').hide()
+    $('#dogs-destroy').hide()
+    $('#dogs-display').hide()
+}
+
+const signOutFailure = function(response) {
+  $('#error-message').text('Sign Out failed, try again')
+  $('form').trigger('reset')
+}
+
+module.exports = {
+  signUpSuccess,
+  signInSuccess,
+  signUpFailure,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
